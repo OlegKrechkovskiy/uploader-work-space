5dcd912e82660000e78a1952f648150c94f8b55a
OlegKrechkovskiy <myx_xa_@mail.ru>
2021-07-08 14:25:28 +0300

    Create work template for uploader

---
+++ uploader-work-space/insales-config.js
@@ -0,0 +1,65 @@
+var extend = require('deepmix')
+var fs = require('fs')
+var imagemin = require('gulp-imagemin')
+var autoprefixer = require('insales-uploader-autoprefixer')
+var jsValidate = require('gulp-jsvalidate');
+
+/**
+ * Активный магазин
+ * Тут нужно указать активную директорию
+ */
+var shop = 'shop_main';
+
+process.argv.forEach((val, index) => {
+  // Проверка параметра с активным магазином в консоли
+  if (~val.indexOf('site=')) {
+    var shopFolder = val.split('=')[1];
+    var stat = fs.statSync(shopFolder);
+    if(stat.isDirectory()) shop = shopFolder;
+  }
+});
+
+/**
+ * Настройки по умолчанию
+ */
+var defaultConfig = {
+  account: {
+    // id: '11111111111111111111111111',
+    // token: '222222222222222222222',
+    // url: 'shop-41254.myinsales.ru',
+    http: true // Если сайт на https и отказывает работать, необходимо выставить false
+  },
+  theme: {
+    //id: '854716',
+    root: './' + shop,
+    backup: true, // Создавать backup после загрузки?
+    assetsSync: true, // Делать синхронизацию с директорией assets?
+  },
+  plugins: {
+    // файлы которые не обрабатываются плагинами
+    exclude: ['*.min.js', '*.min.css', '*.liquid'],
+    // gulp плагины для стилей
+    style: function(stream) {
+        return stream
+          .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: true
+          }))
+    },
+    // gulp плагины для скриптов
+    script: function(stream) {
+        return stream
+          .pipe(jsValidate())
+    },
+    // gulp плагины для изображений
+    img: function(stream) {
+        return stream
+          .pipe(imagemin())
+    }
+  },
+  util: {
+    openBrowser: true // Открывать браузер при запуске стрима?
+  }
+}
+
+module.exports = extend(defaultConfig, require('./'+shop));
